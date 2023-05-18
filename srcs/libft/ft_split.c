ef516271df10b59a90c1385e21e1bc2f3639b2f2
alselnet <alex.selnet@gmail.com>
2023-05-18 20:41:26 +0200

    secured split a bit

--- srcs/libft/ft_split.c
+++ srcs/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: aselnet <aselnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 18:06:15 by aselnet           #+#    #+#             */
-/*   Updated: 2022/05/06 18:23:16 by aselnet          ###   ########.fr       */
+/*   Updated: 2023/05/18 20:36:04 by aselnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,25 +54,22 @@ int	ft_strslen(char const *s, int c)
 	return (slen);
 }
 
-/*void	clear_tab(char **split_tab, unsigned int n)
+char	**clear_tab(char **split_tab, unsigned int n)
 {
 	unsigned int	i;
 
 	i = -1;
 	while (++i < n)
 		free(split_tab[n]);
-}*/
+	return (0);
+}
 
-char	**ft_split(char const *s, char c)
+char	**splitting(char **split_tab, char const *s, char c)
 {
-	char				**split_tab;
 	unsigned int		i;
 	unsigned int		j;
 	unsigned int		n;
 
-	split_tab = (char **) malloc((ft_strslen(s, c) + 1) * sizeof(char *));
-	if (!split_tab)
-		return (0);
 	i = 0;
 	n = -1;
 	while (s[i])
@@ -84,19 +81,35 @@ char	**ft_split(char const *s, char c)
 			j++;
 		i += j;
 		if (j)
+		{
 			split_tab[++n] = ft_make_a_split(s + i - j, j);
+			if (!split_tab[n])
+				return (clear_tab(split_tab, n));
+		}
 	}
 	split_tab[++n] = 0;
 	return (split_tab);
 }
-/*#include <stdio.h>
+
+char	**ft_split(char const *s, char c)
+{
+	char				**split_tab;
+
+	split_tab = (char **) malloc((ft_strslen(s, c) + 1) * sizeof(char *));
+	if (!split_tab)
+		return (0);
+	split_tab = splitting(split_tab, s, c);
+	return (split_tab);
+}
+
+/* #include <stdio.h>
 int	main()
 {
 	char**	split_tab;
 	int	i;
 
 	i = 0;
-	char string[] = "";
+	char string[] = "hey everyone im your new best split !!!\n";
 	split_tab = ft_split(string, ' ');
 	if (!split_tab)
 		return (0);
@@ -107,11 +120,11 @@ int	main()
 		printf("%s\n", split_tab[i]);
 		i++;
 	}
-	while (split_tab[i])
+	while (i >= 0)
 	{
 		free(split_tab[i]);
-		i++;
+		i--;
 	}
 	free(split_tab);
 	return (0);
-}*/
+} */
