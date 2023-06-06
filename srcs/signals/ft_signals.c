6a15b369b9cbd5bb5cbd11ef1d29f7984550f898
oceane-razafy <oceane.razafy@outlook.fr>
2023-06-06 19:46:49 +0200

    remove signals comments

--- srcs/signals/ft_signals.c
+++ srcs/signals/ft_signals.c
@@ -6,36 +6,35 @@
 /*   By: orazafy <orazafy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 19:04:03 by orazafy           #+#    #+#             */
-/*   Updated: 2023/06/06 19:27:58 by orazafy          ###   ########.fr       */
+/*   Updated: 2023/06/06 19:45:10 by orazafy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 
-// #include "minishell.h"
+void	handler_function(int signum, siginfo_t *siginfo, void *ptr)
+{
+	(void)ptr;
+	(void)siginfo;
+	if (signum == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+		g_minishell.exit_status = 1;
+	}
+}
 
-// void	handler_function(int signum, siginfo_t *siginfo, void *ptr)
-// {
-// 	(void)ptr;
-// 	(void)siginfo;
-// 	if (signum == SIGINT)
-// 	{
-// 		write(1, "\n", 1);
-// 		rl_on_new_line();
-// 		rl_replace_line("", 0);
-// 		rl_redisplay();
-// 		g_minishell.exit_status = 1;
-// 	}
-// }
+void	ft_init_signals(void)
+{
+	struct sigaction	sa;
 
-// void	ft_init_signals(void)
-// {
-// 	struct sigaction	sa;
-
-// 	sa.sa_sigaction = handler_function;
-// 	sigemptyset(&sa.sa_mask);
-// 	sa.sa_flags = SA_SIGINFO;
-// 	if (sigaction(SIGINT, &sa, 0) == -1)
-// 		exit(1);
-// 	if (sigaction(SIGQUIT, &sa, 0) == -1)
-// 		exit(1);
-// }
+	sa.sa_sigaction = handler_function;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGINT, &sa, 0) == -1)
+		exit(1);
+	if (sigaction(SIGQUIT, &sa, 0) == -1)
+		exit(1);
+}
