0a9dfff87c53d312e1660f4a2f377ec25326c3f5
oceane-razafy <oceane.razafy@outlook.fr>
2023-07-09 19:33:28 +0200

    outfile permission denied

--- srcs/execution/ft_all_redir.c
+++ srcs/execution/ft_all_redir.c
@@ -6,7 +6,7 @@
 /*   By: orazafy <orazafy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 17:08:02 by orazafy           #+#    #+#             */
-/*   Updated: 2023/07/09 17:14:20 by orazafy          ###   ########.fr       */
+/*   Updated: 2023/07/09 19:28:06 by orazafy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,11 +52,14 @@ void	ft_redir_files(t_cmd *cmd)
 
 void	ft_error_redirections(t_cmd *cmd)
 {
+	if (cmd->fd_out == -1)
+	{
+		write(2, "Permission denied\n", 18);
+		ft_exit_exec(1);
+	}
 	if (cmd->fd_in == -1)
 	{
 		ft_error_no_such_file(cmd->error_infile);
 		ft_exit_exec(1);
-	}
-	if (cmd->fd_out == -1)
-		ft_exit_exec(1);
+	}	
 }
--- srcs/execution/ft_fill_cmd_type_r.c
+++ srcs/execution/ft_fill_cmd_type_r.c
@@ -6,7 +6,7 @@
 /*   By: orazafy <orazafy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 22:46:28 by orazafy           #+#    #+#             */
-/*   Updated: 2023/07/08 22:01:59 by orazafy          ###   ########.fr       */
+/*   Updated: 2023/07/09 19:28:23 by orazafy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,5 @@ void	ft_fill_cmd_for_type_r2(t_cmd *cmd, t_token *lst)
 			cmd->fd_out = open(outfile, O_CREAT | O_WRONLY | O_APPEND, 0664);
 		else
 			cmd->fd_out = open(outfile, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-		if (cmd->fd_out == -1 && cmd->fd_in != -1)
-		{
-			perror("");
-			g_minishell.exit_status = 1;
-		}
 	}
 }
